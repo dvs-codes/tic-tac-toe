fa87620ab88d7a834c8db839deb4cfd277188a18
dvs-codes <123456789+odin@users.noreply.github.com>
2024-01-31 07:56:07 +0530

    working reset button.

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
         board:  [, , , , , , , , ,],
 
         x: 'x',
-        item: 'o',
+        o: 'o',
         lastTurn :'',
         init: function() {
             this.cacheDom()
             this.bindEvents()
-            this.addBox()
         },
         cacheDom: function() {
-            this.gameBoard = document.querySelector('.game-board')   
+            this.gameBoard = document.querySelector('.game-board')  
+            this.resetButton = document.querySelector('.reset')  
         },
         round: function(item, position) {
 
@@ -35,40 +35,46 @@
             } 
 
         },
+
+        addBox: function() {
+
+            //first turn is always x
+            let indexOfBox = gridBox.className; 
+  
+            if (this.lastTurn ==='' || this.lastTurn === this.o) {
+                this.round(this.x,indexOfBox)  
+                gridBox.innerText = this.board[indexOfBox]
+                this.lastTurn = this.x  
+            } else {
+                this.round(this.o,indexOfBox)  
+                gridBox.innerText = this.board[indexOfBox]
+                this.lastTurn = this.o    
+            }
+        
+        },
+
         bindEvents: function() {
+            let gridBoxes =[]
+            
             for (i=0; i<9 ;i++) {
                 let gridBox = document.createElement('button')
                 gridBox.classList.add(`${i}`)
                 this.gameBoard.appendChild(gridBox)
+                gridBoxes.push(gridBox)
             
-                gridBox.addEventListener(('click'), () => {
-                    //first turn is always x
-                    let indexOfBox = gridBox.className; 
-            
-                    if (this.lastTurn ==='' || this.lastTurn === this.item) {
-                        this.round(this.x,indexOfBox)  
-                        gridBox.innerText = this.board[indexOfBox]
-                        this.lastTurn = this.x  
-                    } else {
-                        this.round(this.item,indexOfBox)  
-                        gridBox.innerText = this.board[indexOfBox]
-                        this.lastTurn = this.item     
-                    }
-                })
+                gridBox.addEventListener(('click'), this.addBox)
             
             }
-        
-        },
-        render: function() {
-
-        },
-        addBox: function() {},
+            this.resetButton.addEventListener(('click'), ()=> {
+                this.board = [, , , , , , , , ,]
+                for (i=0; i<9; i++) {
+                    gridBoxes[i].innerText = ''
+                }
+            })
+            
+        }
 
     }
     game.init();
 
 })()
-
-
-
-
